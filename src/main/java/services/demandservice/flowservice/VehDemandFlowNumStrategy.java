5708b09cac294757c36090910bd35d35fb95dc36
Zac <dunhaozhong@gmail.com>
2021-04-25 23:35:55 -0400

    Make "to" field optional

--- src/main/java/services/demandservice/flowservice/VehDemandFlowNumStrategy.java
+++ src/main/java/services/demandservice/flowservice/VehDemandFlowNumStrategy.java
@@ -17,17 +17,22 @@ public class VehDemandFlowNumStrategy implements IVehDemandFlowStrategy {
 
         vehFlows.forEach(vehFlow -> {
             if(vehFlow.getVehsPerHour() >= 0) {
-                root.addElement("flow")
-                    .addAttribute("id", vehFlow.getId())
-                    .addAttribute("type", vehFlow.getVehTypeId())
-                    .addAttribute("from", vehFlow.getFrom())
-                    .addAttribute("begin", String.valueOf(vehFlow.getBegin()))
-                    .addAttribute("end", String.valueOf(vehFlow.getEnd()))
-                    .addAttribute("departSpeed", vehFlow.getDepartSpeed())
-                    .addAttribute("departLane", vehFlow.getDepartLane())
-                    .addAttribute("departPos",String.valueOf(vehFlow.getDepartPos()))
-                    .addAttribute("to", vehFlow.getTo())  // optional
-                    .addAttribute("vehsPerHour", String.valueOf(vehFlow.getVehsPerHour()));
+                String to = vehFlow.getTo();
+                Element flowElement = root.addElement("flow");
+                flowElement.addElement("flow")
+                           .addAttribute("id", vehFlow.getId())
+                           .addAttribute("type", vehFlow.getVehTypeId())
+                           .addAttribute("from", vehFlow.getFrom())
+                           .addAttribute("begin", String.valueOf(vehFlow.getBegin()))
+                           .addAttribute("end", String.valueOf(vehFlow.getEnd()))
+                           .addAttribute("departSpeed", vehFlow.getDepartSpeed())
+                           .addAttribute("departLane", vehFlow.getDepartLane())
+                           .addAttribute("departPos",String.valueOf(vehFlow.getDepartPos()))
+                           .addAttribute("vehsPerHour", String.valueOf(vehFlow.getVehsPerHour()));
+
+                if(to != null && !to.isBlank()){
+                    flowElement.addAttribute("to", to);
+                }
             }
 
         });
--- src/main/java/services/demandservice/flowservice/VehDemandFlowProbStrategy.java
+++ src/main/java/services/demandservice/flowservice/VehDemandFlowProbStrategy.java
@@ -17,17 +17,21 @@ public class VehDemandFlowProbStrategy implements IVehDemandFlowStrategy {
 
         vehFlows.forEach(vehFlow -> {
             if(vehFlow.getProbability() >= 0 && vehFlow.getProbability() <= 1){
-                root.addElement("flow")
-                    .addAttribute("id", vehFlow.getId())
-                    .addAttribute("type", vehFlow.getVehTypeId())
-                    .addAttribute("from", vehFlow.getFrom())
-                    .addAttribute("begin", String.valueOf(vehFlow.getBegin()))
-                    .addAttribute("end", String.valueOf(vehFlow.getEnd()))
-                    .addAttribute("departSpeed", vehFlow.getDepartSpeed())
-                    .addAttribute("departLane", vehFlow.getDepartLane())
-                    .addAttribute("departPos",String.valueOf(vehFlow.getDepartPos()))
-                    .addAttribute("to", vehFlow.getTo())  // optional
-                    .addAttribute("probability", String.valueOf(vehFlow.getProbability()));
+                String to = vehFlow.getTo();
+                Element flowElement = root.addElement("flow");
+                flowElement.addAttribute("id", vehFlow.getId())
+                           .addAttribute("type", vehFlow.getVehTypeId())
+                           .addAttribute("from", vehFlow.getFrom())
+                           .addAttribute("begin", String.valueOf(vehFlow.getBegin()))
+                           .addAttribute("end", String.valueOf(vehFlow.getEnd()))
+                           .addAttribute("departSpeed", vehFlow.getDepartSpeed())
+                           .addAttribute("departLane", vehFlow.getDepartLane())
+                           .addAttribute("departPos",String.valueOf(vehFlow.getDepartPos()))
+                           .addAttribute("probability", String.valueOf(vehFlow.getProbability()));
+
+                if(to != null && !to.isBlank()){
+                    flowElement.addAttribute("to", to);
+                }
             }
         });
 
